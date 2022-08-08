3a74df8bfbbd62dac225211c66629e338d84b6ef
Kevin.Krug <kevin.krug@aperto.com>
2022-08-08 09:07:18 +0200

    :wrench: fix build. use default mongodb uri. todo: setup envs

--- src/server/db.js
+++ src/server/db.js
@@ -1,7 +1,7 @@
 // @todo: create own MongoDB locally
 import mongoose from 'mongoose';
 mongoose.Promise = global.Promise; // use native promises
-mongoose.connect( '',
+mongoose.connect( 'mongodb://localhost',
 {
 	useMongoClient: true,
 } );
